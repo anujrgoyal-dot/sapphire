9284c58c8796e8680aa95e4db67a9b7521e5aa3f
anujrgoyal-dot <anuj.r.goyal@gmail.com>
2026-04-19 00:55:56 +0530

    Update pdfGenerator.js

--- src/lib/pdfGenerator.js
+++ src/lib/pdfGenerator.js
@@ -59,13 +59,7 @@ export function previewSaleOrderPDF(order) {
 }
 
 export function generateSaleOrderPDF(order) {
-  const doc = _buildDoc(order)
-  const rawName = (order.client_snapshot && order.client_snapshot.name) ? order.client_snapshot.name : 'Customer'
-  const clientName = rawName.replace(/[^a-zA-Z0-9 ]/g, '').trim()
-  const soNumber = order.so_number || 'draft'
-  const qtnNum = soNumber.includes('/') ? soNumber.split('/').pop() : soNumber
-  const filename = clientName + '_QTN_' + qtnNum + '.pdf'
-  doc.save(filename)
+  return _buildDoc(order)
 }
 
 function _buildDoc(order) {
